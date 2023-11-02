eef0c586b250abea3e0953f7b8b860831c0e9712
haitd1399 <hai.td1399@gmail.com>
2023-11-03 00:18:02 +0700

    'update'

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import {createApp} from 'vue';
+
+const app = createApp({});
+
+import Navbar from './components/Navbar.vue';
+app.component('navbar-item', Navbar);
+
+import Footer from './components/Footer.vue';
+app.component('footer-item', Footer);
+
+app.mount('#app');
